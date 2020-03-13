3385683ca2a72ea865bf828fc670b3baf373cdc2
François Delapierre <francois.delapierre@youscribe.com>
2020-03-13 10:59:59 +0100

    Add mongodb connexion v2

--- index.js
+++ index.js
@@ -20,10 +20,7 @@ app.get("/", function (req, res) {
 
    // Here we find an appropriate database to connect to, defaulting to
    // localhost if we don't find one.
-   var uristring =
-   process.env.MONGOLAB_URI ||
-   process.env.MONGOHQ_URL ||
-   'mongodb://localhost/HelloMongoose';
+   var uristring =   process.env.MONGOLAB_URI;
 
    // The http server will listen to an appropriate port, or default to
    // port 5000.

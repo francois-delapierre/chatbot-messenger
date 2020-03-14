adade592b4b5878581829d0011305b102c162787
francois-delapierre <francois@delapierre.net>
2020-03-14 15:27:16 +0100

    test mongodb driver

--- index.php
+++ index.php
@@ -15,7 +15,30 @@ $client = new MongoClient($uri, $options);
 
  ?>
 
-mongodb://heroku_4wq6mjkn:cnh4mlou3abk7sf9bbdcvbhvkk@ds037252.mlab.com:37252/heroku_4wq6mjkn
+ <?php
+   # get the mongo db name out of the env
+   $mongo_url = parse_url(getenv("MONGODB_URI"));
+   $dbname = str_replace("/", "", $mongo_url["path"]);
+
+   # connect
+   $m   = new Mongo(getenv("MONGO_URL"));
+   $db  = $m->$dbname;
+   $col = $db->access;
+
+   # insert a document
+   $visit = array( "ip" => $_SERVER["HTTP_X_FORWARDED_FOR"] );
+   $col->insert($visit);
+
+   # print all existing documents
+   $data = $col->find();
+   foreach($data as $visit) {
+     echo "<li>" . $visit["ip"] . "</li>";
+   }
+
+   # disconnect
+   $m->close();
+ ?>
+
 
 </body>
 </html>

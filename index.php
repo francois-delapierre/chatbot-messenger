3986a5998a49deeca362a4d898aa644277612785
francois-delapierre <francois@delapierre.net>
2020-03-14 15:52:33 +0100

    test mongodb

--- index.php
+++ index.php
@@ -4,39 +4,15 @@
 <body>
   Bonjour Paris !
 <?php
-$uri = "mongodb://heroku_4wq6mjkn:cnh4mlou3abk7sf9bbdcvbhvkk@ds037252.mlab.com:37252/heroku_4wq6mjkn";
+$uri = "";
+require 'vendor/autoload.php'; // include Composer's autoloader
 
-/*
- * We recommend explictly configuring a connection timeout (see tips & tricks
- * below). Specify the replica set name to avoid connection errors.
- */
-$options = array("connectTimeoutMS" => 30000, "replicaSet" => "replicaSetName");
-$client = new MongoClient($uri, $options);
+$client = new MongoDB\Client("mongodb://heroku_4wq6mjkn:cnh4mlou3abk7sf9bbdcvbhvkk@ds037252.mlab.com:37252/heroku_4wq6mjkn");
+$collection = $client->demo->beers;
 
- ?>
+$result = $collection->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
 
- <?php
-   # get the mongo db name out of the env
-   $mongo_url = parse_url(getenv("MONGODB_URI"));
-   $dbname = str_replace("/", "", $mongo_url["path"]);
-
-   # connect
-   $m   = new Mongo(getenv("MONGO_URL"));
-   $db  = $m->$dbname;
-   $col = $db->access;
-
-   # insert a document
-   $visit = array( "ip" => $_SERVER["HTTP_X_FORWARDED_FOR"] );
-   $col->insert($visit);
-
-   # print all existing documents
-   $data = $col->find();
-   foreach($data as $visit) {
-     echo "<li>" . $visit["ip"] . "</li>";
-   }
-
-   # disconnect
-   $m->close();
+echo "Inserted with Object ID '{$result->getInsertedId()}'";
  ?>
 
 

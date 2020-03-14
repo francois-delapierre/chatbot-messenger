51e73ac2f0113c83eafd10936813a2b7ad5d7ac2
francois-delapierre <francois@delapierre.net>
2020-03-14 18:57:45 +0100

    mongo connexion test 4

--- index.php
+++ index.php
@@ -12,7 +12,14 @@ require 'vendor/autoload.php';
     try {
       $mongo = new MongoDB\Client(
           'mongodb://heroku_4wq6mjkn:cnh4mlou3abk7sf9bbdcvbhvkk@ds037252.mlab.com:37252/heroku_4wq6mjkn');
-           print_r($mongo->listDatabases());
+
+          $collection = $mongo->heroku_4wq6mjkn->beers;
+
+          $result = $collection->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
+
+          echo "Inserted with Object ID '{$result->getInsertedId()}'";
+
+
      } catch (Exception $e) {
            echo $e->getMessage();
      }

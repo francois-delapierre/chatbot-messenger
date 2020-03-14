d536c42584e80c6de8790dc420011dfe813e0136
francois-delapierre <francois@delapierre.net>
2020-03-14 15:05:42 +0100

    test mongo connexion

--- index.php
+++ index.php
@@ -3,5 +3,19 @@
 </head>
 <body>
   Bonjour Paris !
+<?php
+$uri = "mongodb://heroku_4wq6mjkn:cnh4mlou3abk7sf9bbdcvbhvkk@ds037252.mlab.com:37252/heroku_4wq6mjkn";
+
+/*
+ * We recommend explictly configuring a connection timeout (see tips & tricks
+ * below). Specify the replica set name to avoid connection errors.
+ */
+$options = array("connectTimeoutMS" => 30000, "replicaSet" => "replicaSetName");
+$client = new MongoClient($uri, $options);
+
+ ?>
+
+mongodb://heroku_4wq6mjkn:cnh4mlou3abk7sf9bbdcvbhvkk@ds037252.mlab.com:37252/heroku_4wq6mjkn
+
 </body>
 </html>

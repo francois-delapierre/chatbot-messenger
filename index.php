9caa8add9ff9f4b12933540e5ca23f1d1f0d1fcd
francois-delapierre <francois@delapierre.net>
2020-03-14 15:55:22 +0100

    test mongodb 2

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
   Bonjour Paris !
 <?php
 $uri = "";
-require 'vendor/autoload.php'; // include Composer's autoloader
 
 $client = new MongoDB\Client("mongodb://heroku_4wq6mjkn:cnh4mlou3abk7sf9bbdcvbhvkk@ds037252.mlab.com:37252/heroku_4wq6mjkn");
 $collection = $client->demo->beers;

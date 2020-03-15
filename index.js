2f2245ce8153ddcd2164d33451727979c1619335
francois-delapierre <francois@delapierre.net>
2020-03-15 16:44:11 +0100

    déplacement du module de connexion

--- index.js
+++ index.js
@@ -21,7 +21,13 @@ var theport = process.env.PORT || 5000;
 
 
 
-
+mongoose.connect(uristring, function (err, res) {
+if (err) {
+console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+} else {
+console.log ('Succeeded connected to: ' + uristring);
+}
+});
 
 
 
@@ -131,20 +137,6 @@ function processMessage(senderId,event){
 
     if(messagePayload == "ABONNEMENT")  {
         console.log("Payload détecté : " + messagePayload);
-
-
-
-
-           // Makes connection asynchronously.  Mongoose will queue up database
-           // operations and release them when the connection is complete.
-               mongoose.connect(uristring, function (err, res) {
-               if (err) {
-               console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-               } else {
-               console.log ('Succeeded connected to: ' + uristring);
-               }
-               });
-
 
 
 

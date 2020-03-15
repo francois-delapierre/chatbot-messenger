5d7e344ef2177d734d625e2b414373a4cc52c652
francois-delapierre <francois@delapierre.net>
2020-03-15 18:08:09 +0100

    [PHP] affichage des documents de la collection USERS

--- index.php
+++ index.php
@@ -13,12 +13,14 @@ require 'vendor/autoload.php';
       $mongo = new MongoDB\Client(
           'mongodb://heroku_4wq6mjkn:cnh4mlou3abk7sf9bbdcvbhvkk@ds037252.mlab.com:37252/heroku_4wq6mjkn?retryWrites=false');
 
-          $collection = $mongo->heroku_4wq6mjkn->beers;
+          $collection = $mongo->heroku_4wq6mjkn->users;
 
-          $result = $collection->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
-
-          echo "Inserted with Object ID '{$result->getInsertedId()}'";
-
+          $cursor = $collection->find();
+          foreach ( $cursor as $id => $value )
+          {
+              echo "$id: ";
+              var_dump( $value );
+          }
 
      } catch (Exception $e) {
            echo $e->getMessage();

a2cf4169f97bb3a386d98a9096763a5f21ce047b
François Delapierre <francois.delapierre@youscribe.com>
2020-03-29 19:38:09 +0200

    Désactivation des fonctions Typing_on Typing_off

--- index.js
+++ index.js
@@ -102,8 +102,7 @@ function processPostback(event) {
         var bodyObj = JSON.parse(body);
         name = bodyObj.first_name;
       }
-      activateTyping(senderId);
-      setTimeout(sendGreeting(senderId,name), 3000);
+      sendGreeting(senderId,name);
     });
   }
 
@@ -161,7 +160,6 @@ function processMessage(senderId,event){
               }
               });
 
-        activateTyping(senderId);
         sendSelectCountry(senderId);
       }
 
@@ -191,7 +189,7 @@ function processMessage(senderId,event){
                 });
 
                 var message_a_envoyer = "Ok, tu n'es pas abonné ! Si tu changes d'avis, tu peux toujours utiliser les boutons dans le menu sous la barre de texte ! ;) "
-                activateTyping(senderId);
+
                 sendShortMessage(senderId, message_a_envoyer);
         }
 
@@ -212,7 +210,7 @@ function processMessage(senderId,event){
 
 
         var message_a_envoyer = "D'ailleurs, tu veux bien nous dire quel visuel tu préfèrerais pour l'audiobook ? 🧐 📻 "
-        activateTyping(senderId);
+
         sendShortMessage(senderId, message_a_envoyer);
         sendCarrouselSenegal(senderId);
     }
@@ -220,29 +218,6 @@ function processMessage(senderId,event){
 
   else
   { console.log("Pas de payload détecté");}
-}
-
-
-
-
-function activateTyping(recipientId)
-{
-
-    request({
-      url: "https://graph.facebook.com/v2.6/me/messages",
-      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-      method: "POST",
-      json: {
-        recipient: {id: recipientId}
-      },
-      sender_action:"typing_on"
-    }, function(error, response, body) {
-      if (error) {
-        console.log("Error sending message: " + response.error);
-      }
-    });
-
-
 }
 
 
@@ -263,23 +238,6 @@ function sendShortMessage(recipientId, message) {
       console.log("Error sending message: " + response.error);
     }
   });
-
-
-//Désactivation du typing_on => typing_off
-  request({
-    url: "https://graph.facebook.com/v2.6/me/messages",
-    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-    method: "POST",
-    json: {
-      recipient: {id: recipientId}
-    },
-    sender_action:"typing_off"
-  }, function(error, response, body) {
-    if (error) {
-      console.log("Error sending message: " + response.error);
-    }
-  });
-
 
 }
 
@@ -311,26 +269,6 @@ function sendGreeting(recipientId,userName) {
       console.log("Error sending message: " + response.error);
     }
   });
-
-
-
-  //Désactivation du typing_on => typing_off
-    request({
-      url: "https://graph.facebook.com/v2.6/me/messages",
-      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-      method: "POST",
-      json: {
-        recipient: {id: recipientId}
-      },
-      sender_action:"typing_off"
-    }, function(error, response, body) {
-      if (error) {
-        console.log("Error sending message: " + response.error);
-      }
-    });
-
-
-
 
 }
 
